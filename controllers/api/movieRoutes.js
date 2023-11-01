52b4e8dd9d7e1dc2d399d9fa2a673a30cfb6ea18
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-01 14:56:31 -0500

    update maxbranch 2

--- controllers/api/movieRoutes.js
+++ controllers/api/movieRoutes.js
@@ -1,19 +1,45 @@
 
-const fetch = require('node-fetch');
-const router = require('express').Router();
-const { Movie } = require('../../models')
-const movieData = []
-const userSearch = 'Jaws'
-const url = 'https://movie-database-alternative.p.rapidapi.com/?s='+userSearch+'&r=json&page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '930796b621mshe52735eaff3a7a7p1b11abjsn95e68619975b',
-    'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-  }
-};
+// const fetch = require('node-fetch');
+// const router = require('express').Router();
+// const { Movie } = require('../../models')
+// const movieData = []
+// const userSearch = 'Jaws'
+// const url = 'https://movie-database-alternative.p.rapidapi.com/?s='+userSearch+'&r=json&page=1';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '930796b621mshe52735eaff3a7a7p1b11abjsn95e68619975b',
+//     'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+//   }
+// };
 
-// async function fetchData() {
+// // async function fetchData() {
+// //   try {
+// //     const response = await fetch(url, options);
+// //     const result = await response.json();
+// //     // const jsonResults = result.json();
+// //     // console.log(jsonResults)
+// //     result.Search.forEach(movie => {
+// //       // Create an object for each movie and push it into the array
+// //       movieData.push({
+// //         Title: movie.Title,
+// //         Year: movie.Year,
+// //         imdbID: movie.imdbID,
+// //         Type: movie.Type,
+// //         Poster: movie.Poster
+// //       });
+
+// //     });
+// //     console.log(movieData)
+// //     // make post request to api/movie
+    
+    
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // }
+
+// router.post('/api/movie', async (req, res) => {
 //   try {
 //     const response = await fetch(url, options);
 //     const result = await response.json();
@@ -30,38 +56,63 @@ const options = {
 //       });
 
 //     });
-//     console.log(movieData)
-//     // make post request to api/movie
-    
-    
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
 
-router.post('/api/movie', async (req, res) => {
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// // fetchData();
+// module.exports = Movie;
+
+const fetch = require('node-fetch');
+const router = require('express').Router();
+const { Movie } = require('../../models')
+const movieData = []
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '930796b621mshe52735eaff3a7a7p1b11abjsn95e68619975b',
+    'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+  }
+};
+
+
+router.post('/', async (req, res) => {
   try {
+    const userSearch = req.body.searchTerm; // Get the user's search term from the request body
+
+    if (!userSearch) {
+      return res.status(400).json({ error: "Missing search term" });
+    }
+
+    const url = `https://movie-database-alternative.p.rapidapi.com/?s=${userSearch}&r=json&page=1`;
+
     const response = await fetch(url, options);
     const result = await response.json();
-    // const jsonResults = result.json();
-    // console.log(jsonResults)
+
+    movieData.length = 0;
+
     result.Search.forEach(movie => {
       // Create an object for each movie and push it into the array
       movieData.push({
-        Title: movie.Title,
-        Year: movie.Year,
-        imdbID: movie.imdbID,
-        Type: movie.Type,
-        Poster: movie.Poster
+        title: movie.Title,
+        poster: movie.Poster,
+        release_date: movie.Year,
+        imdb_id: movie.imdbID,
+        type: movie.Type,
       });
-
     });
+    
+    const createdMovies = await Movie.bulkCreate(movieData)
 
+    // Respond with the populated movieData array
+    res.json(createdMovies);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// fetchData();
-module.exports = Movie;
-
+module.exports = router; 
