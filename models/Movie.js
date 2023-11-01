ec6107458e603d2528cd15c9d7c0fdf07ae50b9c
Daniel Sullivan <ojojoj89@gmail.com>
2023-11-01 11:22:32 -0500

    worked on routes and bugfixing

--- models/Movie.js
+++ models/Movie.js
@@ -1,13 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-// const { User, Movie } = require('../models');
-
-
-
-
-
-
-
 
 class Movie extends Model {}
 
@@ -36,29 +28,11 @@ Movie.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
+
+    type: {
       type: DataTypes.STRING,
     },
-    date_created: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
+  
   },
   {
     sequelize,
@@ -66,21 +40,18 @@ Movie.init(
     freezeTableName: true,
     underscored: true,
 
-    modelName: 'movie',
+    modelName: 'Movie',
   }
 );
 
+// bulmaCarousel.attach('#carousel', {
+//   slidesToScroll: 1,
 
-bulmaCarousel.attach('#carousel', {
-  slidesToScroll: 1,
+//   slidestoShow: 4,
 
-  slidestoShow: 4,
+//   navigation: true,
 
-  navigation: true,
-
-  loop: true,
-});
-
+//   loop: true,
+// });
 
 module.exports = Movie;
-
